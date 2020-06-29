4cad9ccf7585860243cf5fdf01e68b4391fbf944
raspimeteo <jjczon@gmail.com>
2020-06-29 15:46:40 -0500

    configured omniauth and first routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+
+  root "session#home"
   resources :items
   resources :orders
   resources :users
+
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+
+  delete '/logout', to: 'session#destroy'
+
+  # GOOGLE Login
+  get '/auth/google_oauth2/callback', to: 'session#oauth_login'
+  # FACEBOOK LOGIN
+  get '/auth/facebook/callback' => 'session#oauth_login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
