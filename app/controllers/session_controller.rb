a1603d21755d9f0b743a161d56964f9daa1f5375
raspimeteo <jjczon@gmail.com>
2020-06-29 16:03:18 -0500

    create homepage

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,5 +1,51 @@
 class SessionController < ApplicationController
-  
-  def home
-  end
+
+	def new
+    end
+    
+
+    def oauth_login
+        user = User.find_or_create_by(email: auth.email)
+        if !user.password_digest
+            user.password = SecureRandom.hex
+            user.name = auth.name
+            user.save
+        end
+        session[:user_id] = user.id
+        redirect_to root_path
+    end
+
+    def create
+        byebug
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message] = "username/password didn't match our records"
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+        session.clear
+        redirect_to '/'
+    end
+
+  	def home
+    	if session[:user_id]
+      		@user = User.find(session[:user_id])
+    	end
+	end
+
+	private
+
+    def auth
+        request.env['omniauth.auth'].info
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+	  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+    before_action :redirect_not_logged_in, only: :show
+
+    def new
+        @user=User.new
+    end
+
+    def create
+        user = User.find_or_create_by(username: user_params[:username])
+        byebug
+        if user
+            user.update(user_params)
+            byebug
+            user.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @user = User.find_by_id(params[:id])
+        redirect_to root_path if !@user
+    end
+    
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :username, :password, :email, :admin)
+    end
 end
