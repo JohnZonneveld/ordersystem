b8db1243a9f772d82a11bc00b212e0c58b7a3f3d
raspimeteo <jjczon@gmail.com>
2020-06-29 15:44:27 -0500

    added relationships to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates_uniqueness_of :name, on: :create, message: "must be unique"
+
+    has_many :orders
+    has_many :order_items, through: :orders
 end
